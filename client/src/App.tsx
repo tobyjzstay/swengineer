b9a9f0967726265aa8940ecdcfcf964859bb816d
Toby <38903351+tobyjzstay@users.noreply.github.com>
2024-07-02 17:35:50 +1200

    Skeleton home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,9 @@
 import { CssBaseline } from "@mui/material";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import createTheme from "@mui/material/styles/createTheme";
-import { SnackbarKey, SnackbarProvider } from "notistack";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { SnackbarAlert } from "./components/SnackbarAlert";
-import Clock from "./routes/Clock";
 import Home from "./routes/Home";
-import Music from "./routes/Music";
-import PageNotFound from "./routes/PageNotFound";
-
-export const snackbars: SnackbarKey[] = [];
-
-interface User {
-    id: string;
-    email: string;
-    created: string;
-}
-
-export const AppContext = React.createContext<App | null>(null);
 
 const darkTheme = createTheme({
     palette: {
@@ -30,42 +15,15 @@ const darkTheme = createTheme({
 });
 
 class App extends React.Component {
-    user: User | null = null;
-    loading = 0;
-
-    startLoading() {
-        this.loading++;
-    }
-
-    stopLoading() {
-        this.loading--;
-    }
-
-    setUser(user: User) {
-        this.user = user;
-    }
-
     render() {
         return (
             <ThemeProvider theme={darkTheme}>
                 <CssBaseline />
-                <SnackbarProvider
-                    Components={{
-                        alert: SnackbarAlert,
-                    }}
-                    transitionDuration={{ enter: 50, exit: 150 }}
-                >
-                    <AppContext.Provider value={this}>
-                        <BrowserRouter>
-                            <Routes>
-                                <Route index element={<Home />} />
-                                <Route path="clock" element={<Clock />} />
-                                <Route path="music" element={<Music />} />
-                                <Route path="*" element={<PageNotFound />} />
-                            </Routes>
-                        </BrowserRouter>
-                    </AppContext.Provider>
-                </SnackbarProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index path="*" element={<Home />} />
+                    </Routes>
+                </BrowserRouter>
             </ThemeProvider>
         );
     }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,30 +1,7 @@
 import { AppBar, Box, Toolbar } from "@mui/material";
-import React from "react";
-
-import { AppContext } from "../App";
 import { Logo } from "./Logo";
 
 function Header() {
-    const appContext = React.useContext(AppContext);
-
-    // React.useEffect(() => {
-    //     getRequest("/auth").then(async (response) => {
-    //         const json = await response.json();
-    //         const { user } = json;
-    //         setUser(user);
-    //     });
-    // }, []);
-
-    React.useEffect(() => {
-        fetch(`/api/auth`, {
-            method: "GET",
-        }).then(async (response) => {
-            const json = await response.json();
-            const { user } = json;
-            appContext?.setUser(user);
-        });
-    }, []);
-
     return (
         <AppBar position="sticky" style={{ background: "transparent", boxShadow: "none" }}>
             <Toolbar variant="dense">

ce605ce158d13b50c056156c7963935a7f060fab
Toby <38903351+tobyjzstay@users.noreply.github.com>
2024-07-23 17:49:58 +1200

    Update server clean script + remove src/public/

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,7 @@
 import express from "express";
-import fs from "node:fs";
 
 const router = express.Router();
 
-const bee = fs.readFileSync("src/public/bee", "utf8");
-
 router.use("/api", require("./api/index"));
-
-router.get("/bee", (_req, res) => {
-    res.send(bee);
-});
 
 module.exports = router;

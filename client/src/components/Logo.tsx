f3d1ec1d2d370330d59e7d4d01f207b75a77265c
Toby <38903351+tobyjzstay@users.noreply.github.com>
2025-01-06 19:41:14 +1300

    Keep lang attribute when clicking on header logo

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Box, Link, Typography } from "@mui/material";
 export function Logo({ scale: s }: { scale?: number }) {
     const scale = s || 1;
     return (
-        <Link href="/" style={{ textDecoration: "none" }}>
+        <Link href={"/" + window.location.search} style={{ textDecoration: "none" }}>
             <Box display="flex" flexDirection="row" alignItems="center">
                 <img src={"/logo.svg"} alt="swengineer" width={64 * scale} style={{ padding: 10 * scale }} />
                 <Typography

f471cb6240ba346d36430d8ad9b732e5ce47cfd7
Toby <38903351+tobyjzstay@users.noreply.github.com>
2025-09-12 11:51:07 +1200

    Add success message enum for ping

--- server/src/routes/ping.ts
+++ server/src/routes/ping.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+export enum SuccessMessage {
+    PONG = "PONG",
+}
+
 router.get("/", (_request, response) => {
-    response.status(200).json({ message: "Pong!" });
+    response.status(200).json({ message: SuccessMessage.PONG });
 });
 
 module.exports = router;

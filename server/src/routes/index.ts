4b482a9ed4097435eec53f74784f3caa01e52bb0
Toby <38903351+tobyjzstay@users.noreply.github.com>
2025-09-16 15:09:09 +1200

    Proxy Google Tag Manager script + make tag ID env variable

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -16,10 +16,27 @@ router.get("/ping", (_request, response) => {
     response.status(200).json({ message: SuccessMessage.PONG });
 });
 
+router.get("/gtm.js", proxyRequest("https://www.googletagmanager.com/gtag/js?id=" + process.env.REACT_APP_GTM_ID));
+
 router.use("/auth", require("./auth").router);
 router.use("/notepad", require("./notepad"));
 
 router.use(require("./404"));
+
+function proxyRequest(upstreamUrl: string) {
+    return async (request: express.Request, response: Response) => {
+        try {
+            const upstreamResponse = await fetch(upstreamUrl);
+            logger.debug(request.method + " " + upstreamResponse.status + " " + upstreamUrl);
+
+            const body = await upstreamResponse.text();
+            response.contentType(upstreamResponse.headers.get("content-type"));
+            response.status(upstreamResponse.status).send(body);
+        } catch (error) {
+            internalServerError(response, new Error("Failed to fetch upstream URL at " + upstreamUrl));
+        }
+    };
+}
 
 export function internalServerError(response: Response, error: unknown) {
     logger.error(error, new Error().stack);

d2fc0c79455c1f2ce0cef74e10cfd9fcda8f2470
Toby <38903351+tobyjzstay@users.noreply.github.com>
2025-09-16 12:07:23 +1200

    Move ping logic into routes index file

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,15 +4,22 @@ import log4js from "log4js";
 const router = express.Router();
 const logger = log4js.getLogger(process.pid.toString());
 
-router.use("/auth", require("./auth").router);
-router.use("/ping", require("./ping"));
-router.use("/notepad", require("./notepad"));
-
-router.use(require("./404"));
+enum SuccessMessage {
+    PONG = "PONG",
+}
 
 enum ServerErrorMessage {
     INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR",
 }
+
+router.get("/ping", (_request, response) => {
+    response.status(200).json({ message: SuccessMessage.PONG });
+});
+
+router.use("/auth", require("./auth").router);
+router.use("/notepad", require("./notepad"));
+
+router.use(require("./404"));
 
 export function internalServerError(response: Response, error: unknown) {
     logger.error(error, new Error().stack);

6caa0ab09a7d21079743f3f0619c4339f1ce828a
Toby <38903351+tobyjzstay@users.noreply.github.com>
2025-09-19 10:27:50 +1200

    Add tests into workflows + make Nodemailer support other SMTPs

--- server/src/nodemailer.ts
+++ server/src/nodemailer.ts
@@ -5,19 +5,24 @@ import { Options } from "nodemailer/lib/mailer";
 const logger = log4js.getLogger(process.pid.toString());
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
     auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD,
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
     },
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
 });
 
 export async function sendMail(options: Options) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { from, ...rest } = options;
     const info = await transporter.sendMail({
-        from: process.env.GMAIL_SENDER,
+        from: process.env.SMTP_SENDER || from,
         ...rest,
     });
-    logger.info(info.messageId + " email sent to " + rest.to);
+
+    if (process.env.NODE_ENV !== "production") {
+        const url = nodemailer.getTestMessageUrl(info);
+        logger.info(url);
+    } else logger.info(info.messageId + " email sent to " + rest.to);
 }
